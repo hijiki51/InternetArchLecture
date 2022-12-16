4da43a85c6aa1916aac8f8e766474bb6e465134e
hijiki51 <hibiki0719euph@gmail.com>
2022-12-16 15:22:22 +0900

    load users from file

--- setup/gcp-tf-cdk/go.mod
+++ setup/gcp-tf-cdk/go.mod
@@ -8,5 +8,6 @@ require github.com/hashicorp/terraform-cdk-go/cdktf v0.14.3
 
 require (
 	github.com/aws/jsii-runtime-go v1.72.0
-	github.com/cdktf/cdktf-provider-google-go/google/v4 v4.0.3 // indirect
+	github.com/cdktf/cdktf-provider-google-go/google/v4 v4.0.3
+	github.com/goccy/go-yaml v1.9.7 // indirect
 )
--- setup/gcp-tf-cdk/main.go
+++ setup/gcp-tf-cdk/main.go
@@ -1,14 +1,61 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/goccy/go-yaml"
+
+	"github.com/cdktf/cdktf-provider-google-go/google/v4/provider"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
+
+type User struct {
+	UserID    string `yaml:"userID"`
+	PublicKey string `yaml:"publicKey"`
+}
+
+var (
+	admin        User
+	participants []User
+)
+
+func loadUsers(file string) error {
+	// yamlファイルを読み込む
+	buf, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	// yamlファイルを構造体に変換する
+	var users struct {
+		Admin        User   `yaml:"admin"`
+		Participants []User `yaml:"participants"`
+	}
+	err = yaml.Unmarshal(buf, &users)
+	if err != nil {
+		return err
+	}
+	admin = users.Admin
+	participants = users.Participants
+
+	return nil
+}
 
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	// The code that defines your stack goes here
+	provider.NewGoogleProvider(scope, jsii.String("InternetArchLecture"), &provider.GoogleProviderConfig{
+		Project: jsii.String("internet-arch-lecture"),
+		Region:  jsii.String("asia-northeast1"),
+	})
+
+	// ユーザー情報を読み込む
+	err := loadUsers("../users.yaml")
+	if err != nil {
+		panic(err)
+	}
 
 	return stack
 }
